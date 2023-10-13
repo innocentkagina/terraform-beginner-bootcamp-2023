cdb6bf4f78eaec64d939e7cd56cc2bc3a72ed6a3
innocentkagina <innocent.kanushu.kagina@gmail.com>
2023-10-13 01:28:48 +0000

    #23 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  cloud {
-      organization = "terraform-beginner-2023"
-
-      workspaces {
-        name = "terra-house-1"
-      }
-    }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-          aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower            = true
@@ -36,14 +7,14 @@ resource "random_string" "bucket_name" {
 }
 
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/example-bucket-policies.html
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 
-}
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
