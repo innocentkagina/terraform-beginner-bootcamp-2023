749b96b8e4ce059ccc94905262f35d695cf9307c
innocentkagina <innocent.kanushu.kagina@gmail.com>
2023-10-14 10:35:29 +0000

    #41 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "terraform-beginner-2023"
@@ -14,6 +20,12 @@ terraform {
 #   }
 #  }
 
+}
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
 module "terrahouse_aws" {
