33311ceb62cad5264049556553cf3b303999afa9
innocentkagina <innocent.kanushu.kagina@gmail.com>
2023-10-15 22:32:33 +0000

    #49 fixed images and changed terratowns category

--- main.tf
+++ main.tf
@@ -42,6 +42,6 @@ resource "terratowns_home" "home" {
 The Flintstones is a classic animated television show that originally aired from 1960 to 1966. It follows the lives of a prehistoric family, the Flintstones, who live in the town of Bedrock. The show is known for its humorous take on modern-day life, using prehistoric versions of technology and everyday items. The main characters include Fred Flintstone, his wife Wilma, their neighbors Barney and Betty Rubble, and their pet dinosaur Dino. The show has become a cultural icon and has been referenced in numerous other works of media.
 DESCRIPTION
   domain_name = module.home_flintstones_hosting.domain_name
-  town = "missingo"
+  town = "video-valley"
   content_version = 1
 }
